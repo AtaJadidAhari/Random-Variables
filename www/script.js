a89fde17bbcbe805efc87d1b3d0f30843e24ffca
garamaleki <alisalmani200149@gmail.com>
2017-12-30 21:05:33 +0330

    Create script.js

---
+++ www/script.js
@@ -0,0 +1,7 @@
+
+	$(window).load(function() {
+		
+		$("button.btn-default").removeClass("btn-default").addClass("btn-warning");
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
